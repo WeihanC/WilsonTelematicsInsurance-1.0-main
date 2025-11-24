3c0d43cb54deec23b692e0cfffaaec6fcf41fe10
Wilson <77941670+WeihanC@users.noreply.github.com>
2025-11-24 02:15:21 -0800

    Update TelematicsAuthManager.swift

--- WilsonTelematicsInsurance/App/TelematicsAuthManager.swift
+++ WilsonTelematicsInsurance/App/TelematicsAuthManager.swift
@@ -13,8 +13,8 @@ final class TelematicsAuthManager {
     static let shared = TelematicsAuthManager()
 
     // 你自己的 InstanceId / InstanceKey（来自 DataHub）
-    private let instanceId  = "33bda6ca-7cbf-4f31-a2c7-e522ccbbd228"
-    private let instanceKey = "2bb56fb7-1460-448e-9712-61bd50bfe22c"
+    private let instanceId  = ""
+    private let instanceKey = ""
 
     private init() {}
 
